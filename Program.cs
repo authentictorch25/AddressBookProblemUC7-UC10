5e4c8cea765c4a636d6f64831be3ae3fb4ee1be8
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-21 00:23:04 +0530

    [Akash]Added program to count the number of persons

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // <copyright file="Program.cs" company="Bridgelabz">
 //   Copyright © 2018 Company
 // </copyright>
-// <creator Name="Akshay"/>
+// <creator Name="Akshay Poriya"/>
 // --------------------------------------------------------------------------------------------------------------------
 namespace AddressBookSystem
 {
@@ -52,7 +52,8 @@ namespace AddressBookSystem
                 Console.WriteLine("\nEnter 1 to add New Address Book \nEnter 2 to Add Contacts \nEnter 3 to Edit Contacts " +
                     "\nEnter 4 to Delete Contacts\nEnter 5 to search contact using city name" +
                     "\nEnter 6 to search contact using state name\nEnter 7 to view contact details by city name" +
-                    "\nEnter 8 to view contact details by state name\nEnter any other key to exit");
+                    "\nEnter 8 to view contact details by state name\nEnter 9 to get number of contacts by city" +
+                    "\nEnter 10 to get number of contacts by state\nEnter any other key to exit");
                 string options = Console.ReadLine();
                 switch (options)
                 {
@@ -79,6 +80,12 @@ namespace AddressBookSystem
                         break;
                     case "8":
                         ViewContactByStateName();
+                        break;
+                    case "9":
+                        NumberOfContactsByCityName();
+                        break;
+                    case "10":
+                        NumberOfContactsByStateName();
                         break;
                     default:
                         flag = false;
@@ -230,7 +237,7 @@ namespace AddressBookSystem
         {
             Console.WriteLine("\nEnter name of the city!");
             string cityName = Console.ReadLine();
-            if (!cityToContactMapperGlobal.ContainsKey(cityName) || cityToContactMapperGlobal[cityName].Count == 0)
+            if (!cityToContactMapperGlobal.ContainsKey(cityName))
             {
                 Console.WriteLine("No record found with such city name!");
                 return;
@@ -249,7 +256,7 @@ namespace AddressBookSystem
         {
             Console.WriteLine("\nEnter name of the State!");
             string stateName = Console.ReadLine();
-            if (!stateToContactMapperGlobal.ContainsKey(stateName) || stateToContactMapperGlobal[stateName].Count == 0)
+            if (!cityToContactMapperGlobal.ContainsKey(stateName))
             {
                 Console.WriteLine("No record found with such state name!");
                 return;
@@ -262,6 +269,30 @@ namespace AddressBookSystem
                 Console.WriteLine("Phone Number: " + contact.phoneNumber + "\nEmail: " + contact.email);
                 Console.WriteLine("\n");
             }
+        }
+
+        public static void NumberOfContactsByCityName()
+        {
+            Console.WriteLine("\nEnter name of the city!");
+            string cityName = Console.ReadLine();
+            if (!cityToContactMapperGlobal.ContainsKey(cityName))
+            {
+                Console.WriteLine("No of Contacts: 0");
+                return;
+            }
+            Console.WriteLine("No of Contacts: " + cityToContactMapperGlobal[cityName].Count);
+        }
+
+        public static void NumberOfContactsByStateName()
+        {
+            Console.WriteLine("\nEnter name of the state!");
+            string stateName = Console.ReadLine();
+            if (!stateToContactMapperGlobal.ContainsKey(stateName))
+            {
+                Console.WriteLine("No of Contacts: 0");
+                return;
+            }
+            Console.WriteLine("No of Contacts: " + stateToContactMapperGlobal[stateName].Count);
         }
     }
 }
